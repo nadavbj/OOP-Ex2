8a9db5cf85b738f3ea23283492bb03e8262cf9c7
nadavbj <nadavbj@gmail.com>
2015-04-11 18:06:35 +0300

    Init some of the files.

---
+++ src/RationalScalar.java
@@ -0,0 +1,28 @@
+
+public class RationalScalar extends Scalar {
+	int a,b;
+	@Override
+	public Scalar add(Scalar s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Scalar mul(Scalar s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Scalar neg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Scalar inv() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/Scalar.java
@@ -0,0 +1,25 @@
+
+public abstract class Scalar {
+	/**
+	 * accepts a scalar argument and returns a
+	 * scalar which is the sum of the current scalar and the argument.
+	 */
+	public abstract Scalar add(Scalar s);
+	/**
+	 * accepts a scalar argument and returns a
+	 * scalar which is the multiplication of the current scalar and the
+	 * argument.
+	 */
+	public abstract Scalar mul(Scalar s);
+	/** 
+	 * @return  a scalar which is the result of multiplying the
+	 * current scalar by (-1).
+	 */
+	public abstract Scalar neg();
+	/**
+	 * @return (1/scalar)
+	 */
+	public abstract Scalar inv();
+
+
+}
