eba3995f8dfa82136429cca632b3df25b5fff1c3
nadav <nadavbj@gmail.com>
2015-04-13 18:26:05 +0300

    Implemented RationalScalar.addScalar.

--- src/RationalScalar.java
+++ src/RationalScalar.java
@@ -1,10 +1,23 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 
 public class RationalScalar extends Scalar {
 	int a,b;
+	public RationalScalar(int a,int b) {
+		this.a=a;
+		if(b==0)
+			throw new RuntimeException("Can't devied by zero");
+		this.b=b;
+	}
 	@Override
 	public Scalar add(Scalar s) {
-		// TODO Auto-generated method stub
-		return null;
+		try{
+			RationalScalar rs=(RationalScalar) s;
+			return new RationalScalar(a*rs.b+b*rs.a,b*rs.b);
+		}
+		catch(Exception e){
+			throw new NotImplementedException();
+		}
 	}
 
 	@Override
